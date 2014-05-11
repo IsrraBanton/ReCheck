e696b423defc8177c0187b742bbb33b81df979b3
IsrraBanton <isrrael_01@yahoo.com>
2014-05-11 11:40:52 -0500

    Primer Commit

---
+++ ReCheck/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReCheck.Startup))]
+namespace ReCheck
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
